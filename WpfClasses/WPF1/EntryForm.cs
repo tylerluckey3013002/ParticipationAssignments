16098e67788cda2770067b82e87e56dd8c0d9608
Tyler Luckey <tylerluckey11@ou.edu>
2020-01-30 12:23:02 -0600

    EntryForm class started

---
+++ WpfClasses/WPF1/EntryForm.cs
@@ -0,0 +1,29 @@
+﻿//Tyler Luckey
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    class EntryForm
+    {
+
+        public string Name { get; set; }
+        public string address { get; set; }
+        public int ZipCode { get; set; }
+
+
+        public EntryForm()
+        {
+
+        }
+
+
+        public EntryForm(string name, string address, int zipcode)
+        {
+
+        }
+    }
+}
